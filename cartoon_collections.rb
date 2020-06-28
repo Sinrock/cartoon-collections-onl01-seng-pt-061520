0284c660f4cf4f135ede5b30d47a6cbcf4d69c4b
Michael R <mruhaak@gmail.com>
2020-06-28 06:49:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,14 +22,11 @@ end
 
 summon_captain_planet(planeteer_calls)
 
-short_words = ["puff", "go", "two"]
-assorted_words = ["two","go","industrious","bop"]
-def long_planeteer_calls(short_words)
-  if short_words.each.length >= 4
-    return false
-  else true
-  end
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
 end
+
+long_planeteer_calls(planeteer_calls)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
